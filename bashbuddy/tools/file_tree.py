6afdeb7b41cc6be3df9bc4a0cecafc489c6dd164
Omegastick <isaac@poulton.dev>
2023-06-14 20:20:42 +0100

    Make file tree more robust

--- bashbuddy/tools/file_tree.py
+++ bashbuddy/tools/file_tree.py
@@ -2,11 +2,19 @@ import os
 
 from bashbuddy.tools.persistent_bash import PersistentBash
 
+TRUNCATED_MESSAGE = "Truncated due to too many items..."
+
 
 def generate_file_tree(root_dir: str) -> dict:
+    if root_dir.endswith(os.sep):
+        root_dir = root_dir[:-1]
+
     file_tree: dict = {root_dir: {}}
 
     for root, dirs, files in os.walk(root_dir):
+        if root.split(os.sep)[-1].startswith("."):
+            continue
+
         path_parts = root.replace(root_dir, "").split(os.sep)[1:]
 
         parent_dict = file_tree[root_dir]
@@ -28,37 +36,78 @@ def generate_file_tree(root_dir: str) -> dict:
     return file_tree
 
 
-def format_file_tree(file_tree: dict, parent_path=".") -> str:
+def format_file_tree(file_tree: dict, parent_path: str = ".", max_items_per_directory: int = 50) -> str:
+    """
+    Given a file tree as a nested dict, return a string representation of the tree, formatted as follows:
+
+    # /path/to/directory
+    dir1/
+    file1.txt
+    # /path/to/directory/dir1
+    file2.txt
+
+    If a directory contains more than `max_items_per_directory`, the directory listing is truncated.
+    """
+    if parent_path.endswith(os.sep):
+        parent_path = parent_path[:-1]
+
     formatted_tree = f"# {parent_path}\n"
 
     if parent_path in file_tree:
         file_tree = file_tree[parent_path]
 
-    keys = sorted(file_tree.keys(), key=lambda x: (not isinstance(file_tree[x], dict), x))
+    keys = get_sorted_keys(file_tree)
+
+    formatted_tree += format_directory(file_tree, keys, max_items_per_directory)
 
     for key in keys:
         if isinstance(file_tree[key], dict):
-            formatted_tree += key + "/\n"
-        else:
-            formatted_tree += key + "\n"
-
-    formatted_tree += "###\n"
-
-    for key in keys:
-        if isinstance(file_tree[key], dict):
-            formatted_tree += format_file_tree(file_tree[key], os.path.join(parent_path, key))
+            formatted_tree += format_file_tree(file_tree[key], os.path.join(parent_path, key), max_items_per_directory)
 
     return formatted_tree
+
+
+def get_sorted_keys(file_tree: dict) -> list:
+    """Return a sorted list of keys from a directory in the file tree."""
+    return sorted(file_tree.keys(), key=lambda x: (not isinstance(file_tree[x], dict), x))
+
+
+def format_directory(file_tree: dict, keys: list, max_items_per_directory: int) -> str:
+    """Format the directory listing, and truncate if it exceeds `max_items_per_directory`."""
+    formatted_listing = ""
+    item_count = 0
+
+    for key in keys:
+        if item_count >= max_items_per_directory:
+            formatted_listing += TRUNCATED_MESSAGE + "\n"
+            return formatted_listing
+
+        if isinstance(file_tree[key], dict):
+            formatted_listing += key + "/\n"
+        else:
+            formatted_listing += key + "\n"
+
+        item_count += 1
+
+    return formatted_listing
 
 
 class FileTree:
     def __init__(self, persistent_bash: PersistentBash):
         self.bash = persistent_bash
 
-    def get(self, path: str = ".") -> str:
+    def _truncate_file_tree(self, file_tree: str, max_lines: int) -> str:
+        lines = file_tree.split("\n")
+        if len(lines) > max_lines:
+            return "\n".join(lines[:max_lines]) + "\n" + TRUNCATED_MESSAGE + "\n"
+        return file_tree
+
+    def get(self, path: str = ".", max_lines=100) -> str:
         if path == ".":
             current_dir = self.bash.run("pwd").strip()
         else:
             current_dir = os.path.join(self.bash.run("pwd").strip(), path)
         file_tree = generate_file_tree(current_dir)
-        return format_file_tree(file_tree, current_dir)
+        formatted_file_tree = format_file_tree(file_tree, current_dir)
+        truncated_file_tree = self._truncate_file_tree(formatted_file_tree, max_lines)
+        return truncated_file_tree
--- tests/tools/test_file_tree.py
+++ tests/tools/test_file_tree.py
@@ -3,7 +3,7 @@ from unittest.mock import MagicMock
 import pytest
 from pyfakefs.fake_filesystem import FakeFilesystem
 
-from bashbuddy.tools.file_tree import FileTree, format_file_tree, generate_file_tree
+from bashbuddy.tools.file_tree import FileTree, format_directory, format_file_tree, generate_file_tree, get_sorted_keys
 from bashbuddy.tools.persistent_bash import PersistentBash
 
 
@@ -15,9 +15,9 @@ def mock_bash():
 @pytest.fixture
 def fs(fs: FakeFilesystem) -> FakeFilesystem:
     fs.create_dir("/test")
-    fs.create_file("/test/file1.txt", contents="File 1 content")
-    fs.create_file("/test/dir1/file2.txt", contents="File 2 content")
-    fs.create_file("/test/dir1/dir2/file3.txt", contents="File 3 content")
+    fs.create_file("/test/file1.txt")
+    fs.create_file("/test/dir1/file2.txt")
+    fs.create_file("/test/dir1/dir2/file3.txt")
     return fs
 
 
@@ -27,12 +27,75 @@ def test_generate_file_tree(fs: FakeFilesystem):
     assert file_tree == expected_tree
 
 
+def test_generate_file_tree_ignores_hidden_files(fs: FakeFilesystem):
+    fs.create_file("/test/.hidden.txt")
+    file_tree = generate_file_tree("/test")
+    expected_tree = {"/test": {"dir1": {"dir2": {"file3.txt": None}, "file2.txt": None}, "file1.txt": None}}
+    assert file_tree == expected_tree
+
+
+def test_generate_file_tree_ignores_hidden_directories(fs: FakeFilesystem):
+    fs.create_dir("/test/.hidden")
+    fs.create_file("/test/.hidden/file.txt")
+    file_tree = generate_file_tree("/test")
+    expected_tree = {"/test": {"dir1": {"dir2": {"file3.txt": None}, "file2.txt": None}, "file1.txt": None}}
+    assert file_tree == expected_tree
+
+
+def test_generate_file_tree_handles_trailing_slash(fs: FakeFilesystem):
+    file_tree = generate_file_tree("/test/")
+    expected_tree = {"/test": {"dir1": {"dir2": {"file3.txt": None}, "file2.txt": None}, "file1.txt": None}}
+    assert file_tree == expected_tree
+
+
 def test_format_file_tree(fs: FakeFilesystem):
     file_tree = {"/test": {"dir1": {"dir2": {"file3.txt": None}, "file2.txt": None}, "file1.txt": None}}
     formatted_tree = format_file_tree(file_tree, "/test")
-    expected_tree = (
-        "# /test\ndir1/\nfile1.txt\n###\n# /test/dir1\ndir2/\nfile2.txt\n###\n# /test/dir1/dir2\nfile3.txt\n###\n"
-    )
+    expected_tree = """# /test
+dir1/
+file1.txt
+# /test/dir1
+dir2/
+file2.txt
+# /test/dir1/dir2
+file3.txt
+"""
+    assert formatted_tree == expected_tree
+
+
+def test_format_file_tree_truncates_if_too_many_lines(fs: FakeFilesystem):
+    file_tree = {"/test": {"file1.txt": None, "file2.txt": None, "file3.txt": None, "file4.txt": None}}
+    formatted_tree = format_file_tree(file_tree, "/test", max_items_per_directory=3)
+    expected_tree = """# /test
+file1.txt
+file2.txt
+file3.txt
+Truncated due to too many items...
+"""
+    assert formatted_tree == expected_tree
+
+
+def test_nested_large_file_tree(fs: FakeFilesystem):
+    file_tree = {
+        "/test": {
+            "dir1": {
+                "dir2": {"file1.txt": None, "file2.txt": None, "file3.txt": None, "file4.txt": None},
+            },
+            "file5.txt": None,
+        }
+    }
+    formatted_tree = format_file_tree(file_tree, "/test", max_items_per_directory=3)
+    expected_tree = """# /test
+dir1/
+file5.txt
+# /test/dir1
+dir2/
+# /test/dir1/dir2
+file1.txt
+file2.txt
+file3.txt
+Truncated due to too many items...
+"""
     assert formatted_tree == expected_tree
 
 
@@ -40,7 +103,32 @@ def test_get_file_tree(mock_bash, fs: FakeFilesystem):
     mock_bash.run.return_value = "/test"
     file_tree = FileTree(mock_bash)
     result = file_tree.get()
-    expected_result = (
-        "# /test\ndir1/\nfile1.txt\n###\n# /test/dir1\ndir2/\nfile2.txt\n###\n# /test/dir1/dir2\nfile3.txt\n###\n"
-    )
+    expected_result = """# /test
+dir1/
+file1.txt
+# /test/dir1
+dir2/
+file2.txt
+# /test/dir1/dir2
+file3.txt
+"""
     assert result == expected_result
+
+
+def test_format_directory(fs: FakeFilesystem):
+    file_tree = {"/test": {"file1.txt": None, "file2.txt": None, "file3.txt": None, "file4.txt": None}}
+    keys = sorted(file_tree["/test"].keys(), key=lambda x: (not isinstance(file_tree["/test"][x], dict), x))
+    formatted_listing = format_directory(file_tree["/test"], keys, 3)
+    expected_listing = """file1.txt
+file2.txt
+file3.txt
+Truncated due to too many items...
+"""
+    assert formatted_listing == expected_listing
+
+
+def test_get_sorted_keys():
+    file_tree = {"/test": {"dir1": {"dir2": {"file3.txt": None}, "file2.txt": None}, "file1.txt": None}}
+    keys = get_sorted_keys(file_tree["/test"])
+    expected_keys = ["dir1", "file1.txt"]
+    assert keys == expected_keys
